14d183000a3f0a269f56d17a30027d89c1b26c80
haodan <hzhaodan@corp.netease.com>
2019-05-09 14:50:57 +0800

    test

---
+++ geekTimeStudy/MyBlock.h
@@ -0,0 +1,17 @@
+//
+//  MyBlock.h
+//  geekTimeStudy
+//
+//  Created by 郝丹 on 2019/5/9.
+//  Copyright © 2019年 郝丹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyBlock : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
